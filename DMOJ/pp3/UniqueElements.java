a0e8357b19d6e7c94a62fa7d36e25a0ccebaa3ae
JasonChen1203 <71620462+JasonChen1203@users.noreply.github.com>
2022-01-16 10:34:34 -0500

    added pp3 DMOJ code

---
+++ DMOJ/pp3/UniqueElements.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws IOException {
+		HashSet<Integer> hset = new HashSet<Integer>();
+		int n = readInt();
+		
+		for(int i = 0; i < n; i++) {
+			hset.add(readInt());
+		}
+		
+		System.out.println(hset.size());
+	}
+	
+	
+	
+	static String next() throws IOException {
+		while(st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		
+		return st.nextToken();
+	}
+	
+	
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	
+	
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	
+	
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
