1355509d9530dd0ae463a3eafa6d0cca99b5cfc6
JasonChen1203 <71620462+JasonChen1203@users.noreply.github.com>
2022-01-24 13:56:39 -0500

    added CCC code

---
+++ CCC/Junior/CCC'01J2-ModInverse.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws IOException {
+		int x = readInt();
+		int m = readInt();
+		int modInverse = 0;
+		
+		for(int i = 0; i < m; i++) {
+			int product = x * i;
+			
+			if(product % m == 1) {
+				modInverse = i;
+				break;
+			}
+		}
+		
+		if(modInverse != 0) {
+			System.out.println(modInverse);
+		} else {
+			System.out.println("No such integer exists.");
+		}
+	}
+	
+	
+	
+	static String next() throws IOException {
+		while(st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		
+		return st.nextToken();
+	}
+	
+	
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	
+	
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	
+	
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
