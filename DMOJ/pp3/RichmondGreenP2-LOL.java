a7bd086969090773e01d4e732aff7b9b1b8a40fd
JasonChen1203 <71620462+JasonChen1203@users.noreply.github.com>
2022-03-31 10:45:03 -0400

    added DMOJ code

---
+++ DMOJ/pp3/RichmondGreenP2-LOL.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws IOException {
+		int N = readInt();
+		int[] x = new int[N];
+		int[] y = new int[N];
+		int length; int width;
+		int area;
+		
+		for(int i = 0; i < N; i++) {
+			x[i] = readInt();
+			y[i] = readInt();
+		}
+		
+		Arrays.sort(x);
+		Arrays.sort(y);
+		
+		length = x[N - 1] - x[0];
+		width = y[N - 1] - y[0];
+		area = length * width;
+		
+		System.out.println(area);
+	}
+	
+	
+	
+	static String next() throws IOException {
+		while(st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		
+		return st.nextToken();
+	}
+	
+	
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	
+	
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	
+	
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
