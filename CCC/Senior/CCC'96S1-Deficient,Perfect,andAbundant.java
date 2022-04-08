bd34963a9663def739f716f045f8c2935384a665
JasonChen1203 <71620462+JasonChen1203@users.noreply.github.com>
2022-04-07 20:31:49 -0400

    added CCC code

---
+++ CCC/Senior/CCC'96S1-Deficient,Perfect,andAbundant.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws IOException {
+		int N = readInt();
+		int[] array = new int[N];
+		int[] numberArray = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			int number = readInt();
+			numberArray[i] = number;
+			int sum = 0;
+			
+			for(int j = 1; j < number; j++) {
+				if(number % j == 0) {
+					sum += j;
+				}
+			}
+			
+			if(sum == number) {
+				array[i] = 0;
+			} else if(sum < number) {
+				array[i] = 1;
+			} else {
+				array[i] = 2;
+			}
+		}
+		
+		for(int i = 0; i < N; i++) {
+			if(array[i] == 0) {
+				System.out.println(numberArray[i] + " is a perfect number.");
+			} else if(array[i] == 1) {
+				System.out.println(numberArray[i] + " is a deficient number.");
+			} else {
+				System.out.println(numberArray[i] + " is an abundant number.");
+			}
+		}
+	}
+	
+	
+	
+	static String next() throws IOException {
+		while(st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		
+		return st.nextToken();
+	}
+	
+	
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	
+	
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	
+	
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
