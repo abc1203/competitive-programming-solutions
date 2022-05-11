f5695c806c524ae5f7aa0d3f4cf95312dcf8eb0a
JasonChen1203 <71620462+JasonChen1203@users.noreply.github.com>
2022-05-11 16:04:59 -0400

    added CCC code

---
+++ CCC/Junior/CCC'19J2-TimeToDecompress.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static PrintWriter pr = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+	static StringTokenizer st;
+	
+	
+	public static void main(String[] args) throws IOException {
+		int L = readInt();
+		String[] array = new String[L];
+		
+		for(int i = 0; i < L; i++) {
+			int N = readInt();
+			String symbol = next();
+			String pattern = "";
+			
+			for(int j = 0; j < N; j++) {
+				pattern += symbol;
+			}
+			
+			array[i] = pattern;
+		}
+		
+		for(int i = 0; i < L; i++) {
+			System.out.println(array[i]);
+		}
+	}
+	
+	
+	
+	static String next() throws IOException {
+		while(st == null || !st.hasMoreTokens()) {
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		
+		return st.nextToken();
+	}
+	
+	
+	static int readInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	
+	static double readDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+	
+	
+	static long readLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	
+	static char readCharacter() throws IOException {
+		return next().charAt(0);
+	}
+	
+	
+	static String readLine() throws IOException {
+		return br.readLine().trim();
+	}
+}
